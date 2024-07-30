942570ab8d3230920b9f1be0d15d085cb6f49d38
LuaMedeiros <92182840+LuaMedeiros@users.noreply.github.com>
2024-07-29 21:49:28 -0300

    Update Jump_Game.cpp

--- LeetCode_55/Jump_Game.cpp
+++ LeetCode_55/Jump_Game.cpp
@@ -3,12 +3,19 @@
 class Solution {
 public:
     bool canJump(std::vector<int>& nums) {
+        // O objetivo inicial é alcançar o último índice da matriz
         int goal = nums.size() - 1;
+        
+        // Vamos percorrer a matriz de trás para frente, começando do penúltimo índice
         for (int i = nums.size() - 2; i >= 0; --i) {
+            // Se podemos pular do índice atual (i) para o objetivo atual ou além
             if (i + nums[i] >= goal) {
+                // Então, redefinimos o objetivo para o índice atual
                 goal = i;
             }
         }
+        
+        // No final, verificamos se conseguimos redefinir o objetivo para o início da matriz
         return goal == 0;
     }
 };
